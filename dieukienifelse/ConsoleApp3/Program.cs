086e08df9a4936fdec0933570d8a46ad52f727fe
= <=>
2019-11-19 13:49:26 +0700

    c#

---
+++ dieukienifelse/ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = 200;
+            if(a==100)
+            {
+                if(b==200)
+                {
+                    Console.WriteLine("gia tri cua a la 100 b la 200");
+                }
+            }
+            Console.WriteLine("gia tri cua a la {0}", a);
+            Console.WriteLine("gia tri cua b la {0}", b);
+        }
+    }
+}
