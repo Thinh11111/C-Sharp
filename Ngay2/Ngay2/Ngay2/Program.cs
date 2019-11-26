7e1026e66dfaf697850965ee056bc82cae2a4f4b
Thanh Lam <thanhlam1702@gmail.com>
2019-11-26 13:30:53 +0700

    Bai ngay 2

---
+++ Ngay2/Ngay2/Ngay2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ngay2
+{
+    class Program
+    {
+        static void VD1()
+        {
+            Student[] DSSV;
+            int n;
+            Console.WriteLine("Nhập số lượng sinh viên:");
+            n = int.Parse(Console.ReadLine());
+            DSSV = new Student[n];
+            Console.WriteLine("--------NHẬP DANH SÁCH SINH VIÊN---------");
+            for (int i = 0; i < n; i++)
+            {
+                DSSV[i] = new Student();
+                Console.WriteLine("Nhập MaSV {0}:", i + 1);
+                DSSV[i].StudentID = int.Parse(Console.ReadLine());
+                Console.WriteLine("Nhập họ tên SV:");
+                DSSV[i].Name = Console.ReadLine();
+                Console.WriteLine("Nhập khoa:");
+                DSSV[i].Faculty = Console.ReadLine();
+                Console.WriteLine("Nhập DTB:");
+                DSSV[i].Mark = float.Parse(Console.ReadLine());
+            }
+
+            //Xuất DS Sinh viên
+            Console.WriteLine("-----------Xuất danh sách sinh viên-----------");
+            foreach (Student sv in DSSV)
+            {
+                sv.Show();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            VD1();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ngay2/Ngay2/Ngay2/Student.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ngay2
+{
+    class Student
+    {
+        private int SID;
+        private string TenSV;
+        private string Khoa;
+        private float DiemTB;
+        public Student()
+        {
+            SID = 1;
+            TenSV = "Nguyen Van Nam";
+            Khoa = "CNTT";
+            DiemTB = 7;
+        }
+        public Student(Student stu)
+        {
+            SID = stu.SID;
+            TenSV = stu.TenSV;
+            Khoa = stu.Khoa;
+            DiemTB = stu.DiemTB;
+        }
+        public Student(int id, string ten, string khoa, float dtb)
+        {
+            this.SID = id;
+            this.TenSV = ten;
+            this.Khoa = khoa;
+            this.DiemTB = dtb;
+        }
+        public int StudentID
+        {
+            get { return SID; }
+            set { SID = value; }
+        }
+        public string Name
+        {
+            get { return TenSV; }
+            set { TenSV = value; }
+        }
+        public string Faculty
+        {
+            get { return Khoa; }
+            set { Khoa = value; }
+        }
+        public float Mark
+        {
+            get { return DiemTB; }
+            set { DiemTB = value; }
+        }
+        public void Show()
+        {
+            Console.WriteLine("MSVV:{0}", this.SID);
+            Console.WriteLine("Tên SV:{0}", this.TenSV);
+            Console.WriteLine("Khoa:{0}", this.Khoa);
+            Console.WriteLine("Điểm Trung Bình:{0}", this.DiemTB);
+        }
+    }
+}
