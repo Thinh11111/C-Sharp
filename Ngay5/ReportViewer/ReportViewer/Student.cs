80c535c3c84b9f15f27020a306a25d6e367ec4d3
Thanh Lam <thanhlam1702@gmail.com>
2019-12-17 15:04:36 +0700

    Do An

---
+++ Ngay5/ReportViewer/ReportViewer/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportViewer
+{
+    public class Student
+    {
+        public string StudentID { get; set; }
+        public string FullName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string Address { get; set; }
+        public Student()
+        { }
+        public Student(string id, string name, DateTime dtBirthday, string add)
+        {
+            StudentID = id;
+            FullName = name;
+            BirthDay = dtBirthday;
+            Address = add;
+        }
+    }
+}
